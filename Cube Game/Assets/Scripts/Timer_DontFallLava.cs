458c6fbbdaefe23f8045f1f9a55e6f1e148fed4c
josem <josema.pardo@oj2medial.com>
2021-12-01 00:19:16 -0500

    added timer in don't fall in lava

---
+++ Cube Game/Assets/Scripts/Timer_DontFallLava.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer_DontFallLava : MonoBehaviour
+{
+
+    public Text timerText;
+
+    private float startTime;
+
+    private bool finished = false;
+
+    public Text bestTime;
+
+    string jj;
+
+    float kk;
+
+    public bool reset = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+
+        bestTime.text = PlayerPrefs.GetString("BestTime1", "0").ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(finished)
+        return;
+
+        float t = Time.time - startTime;
+
+        int minutes = ((int) t / 60);
+
+        float seconds = (t % 60);
+        
+        timerText.text = (minutes + ":" + seconds).ToString();
+
+        jj = timerText.text;
+
+    }
+
+    public void Finish()
+    {
+        finished = true;
+        timerText.color = Color.yellow;
+
+        
+            PlayerPrefs.SetString("BestTime1",jj);
+            bestTime.text = jj.ToString();
+
+            
+        
+    }
+}
