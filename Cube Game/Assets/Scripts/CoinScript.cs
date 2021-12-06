9c46b2317de4a7b5c830e236a1dd6b30cc2b0cdc
Sophie Lehmann <sophie.lehmann@ue-germany.de>
2021-12-06 23:41:08 +0100

    collect coin and show score
    You can know collect coins the ammount will be shown in the top left corner

--- Cube Game/Assets/Scripts/CoinScript.cs
+++ Cube Game/Assets/Scripts/CoinScript.cs
@@ -13,6 +13,15 @@ public class CoinScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       transform.Rotate(90 * Time.deltaTime,0, 0);
     }
+	
+	 private void OnTriggerEnter(Collider other)
+		{
+			if(other.name == "Player")
+			{
+				other.GetComponent<PointsScript>().points++; // add 1 to points
+				Destroy(gameObject);
+			}
+		}
 }
