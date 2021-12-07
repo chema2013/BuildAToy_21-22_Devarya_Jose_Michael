835163b22239ec6a0794527f82eaee964895d29d
richardsondavid <david.robert@ue-germany.de>
2021-12-07 15:37:47 +0100

    MouseDrag to MouseClick maybe?

--- Cube Game/Assets/Scripts/TerrainRotation.cs
+++ Cube Game/Assets/Scripts/TerrainRotation.cs
@@ -6,7 +6,7 @@ public class TerrainRotation : MonoBehaviour
 {
     float rotationSpeed = 1f;
 
-    private void OnMouseDrag()
+    private void OnMouseClick()
     {
         float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
         float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
