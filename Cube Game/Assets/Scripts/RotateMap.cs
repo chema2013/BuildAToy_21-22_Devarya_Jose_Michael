54d3174f4e18131107066d203722b6e7e66c9117
richardsondavid <david.robert@ue-germany.de>
2021-12-08 10:29:27 +0100

    Added map rotation

--- Cube Game/Assets/Scripts/RotateMap.cs
+++ Cube Game/Assets/Scripts/RotateMap.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateMap : MonoBehaviour
 {
+    [SerializeField] float rotationSpeed = 10f;
+    bool dragging = false;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDrag()
+    {
+        dragging = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+        }
+
+        void FixedUpdate()
+        {
+            if (dragging)
+            {
+                float x = Input.GetAxis("Mouse X") * rotationSpeed * Time.fixedDeltaTime;
+                float y = Input.GetAxis("Mouse Y") * rotationSpeed * Time.fixedDeltaTime;
+            }
+        }
     }
 }
